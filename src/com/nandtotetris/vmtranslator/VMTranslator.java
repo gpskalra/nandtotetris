8824e506632f347dd301965f9318e1c262d69e1a
gaganpreet1810 <gaganpreet1810@gmail.com>
2016-03-23 19:23:59 +0530

    change input validation and get output file code

--- src/com/nandtotetris/vmtranslator/VMTranslator.java
+++ src/com/nandtotetris/vmtranslator/VMTranslator.java
@@ -5,70 +5,71 @@ import java.io.File;
 public class VMTranslator {
 
     /**
-     * Validates that a file has extension vm
+     * returns true if an input file has extension vm
+     *
      * @param file File object for the input file
      */
-    public void validateExtension(File file) {
+    public boolean isAVmFile(File file) {
 
         String fileName = file.getAbsolutePath();
         String testExtensionPattern = "(.*)(\\.vm)";
 
-        if(!fileName.matches(testExtensionPattern)) {
-            System.out.println("Error: The input file does not have a .vm extension");
-            System.out.println("Exiting");
-            System.exit(1);
-        }
+        return fileName.matches(testExtensionPattern);
     }
 
     /**
-     * Checks if the files have necessary vm extensions
-     * If the input corresponds to a directory, checks if all
-     * files contained in it have vm extension. In case the
+     * Validates the input and returns the file object for
+     * the output file.
+     *
+     * If the input corresponds to a directory, checks if
+     * it contains at least one vm file. In case the
      * input is a file, checks that the file has vm extension.
+     * If any check fails, it exits the program.
+     *
+     * If the input is a directory, appends ".asm" to the dir
+     * name to get the output file name. If the input is a file,
+     * replaces ".vm" with ".asm"
      *
      * @param input the input File object
-     */
-    public void validateExtensions(File input) {
-
-        if(input.isDirectory()) {
-
-            File[] listOfInputFiles = input.listFiles();
-
-            if(listOfInputFiles==null) {
-                System.out.println("Error: The input directory is empty");
-                System.out.println("Exiting");
-                System.exit(1);
-            }
-
-            for(File file:listOfInputFiles) {
-                validateExtension(file);
-            }
-
-        } else {
-            validateExtension(input);
-        }
-
-    }
-
-    /**
-     * Returns the file object for the output file
-     * by appending .asm to the input dir/file.
-     *
-     * @param input File object for the input file/dir
      * @return File object for the output file
      */
-    File getOutputFile(File input) {
+    public File validateInputAndGetOutputFile(File input) {
 
         File outputFile;
 
         if(input.isDirectory()) {
 
-            outputFile = new File(input.getAbsolutePath().concat(".asm"));
+            File[] listOfInputFiles = input.listFiles();
 
-        } else {
+            boolean seenAVmFile = false;
+            for(File file:listOfInputFiles) {
+
+                if (isAVmFile(file)) {
+                    seenAVmFile = true;
+                    break;
+                }
+
+            }
+
+            if (!seenAVmFile) {
+                System.out.println("Error: The input directory does not have a vm file");
+                System.out.println("Exiting");
+                System.exit(1);
+            }
+
+            outputFile = new File(input.getAbsolutePath() + File.separator + input.getName() + ".asm");
+
+        }
+
+        else {
+
+            if (!isAVmFile(input)) {
+                System.out.println("Error: The input file does not have a vm extension");
+                System.out.println("Exiting");
+                System.exit(1);
+            }
 
             outputFile = new File(input.getAbsolutePath().replaceAll("\\.vm",".asm"));
-
         }
 
         return outputFile;
@@ -87,86 +88,91 @@ public class VMTranslator {
      */
     public void translateFile(File inputFile, CodeWriter codeWriter) {
 
-        codeWriter.setCurrentFile(inputFile);
-        codeWriter.emitComment("file: " + inputFile.getName());
+        if(isAVmFile(inputFile)) {
 
-        Parser parser = new Parser(inputFile);
+            codeWriter.setCurrentFile(inputFile);
+            codeWriter.emitComment("file: " + inputFile.getName());
 
-        while(parser.hasMoreCommands()) {
+            Parser parser = new Parser(inputFile);
 
-            parser.advance();
+            while(parser.hasMoreCommands()) {
 
-            // Write the command as a comment
-            // Helps in debugging
-            codeWriter.emitComment(parser.getCurrentCommand());
+                parser.advance();
 
-            switch (parser.commandType()) {
+                // Write the command as a comment
+                // Helps in debugging
+                codeWriter.emitComment(parser.getCurrentCommand());
 
-                case C_ARITHMETIC:
+                switch (parser.commandType()) {
 
-                    codeWriter.writeArithmetic(parser.arg1());
-                    break;
+                    case C_ARITHMETIC:
 
-                case C_PUSH:
-                case C_POP:
+                        codeWriter.writeArithmetic(parser.arg1());
+                        break;
 
-                    codeWriter.writePushPop(parser.commandType(),parser.arg1(),parser.arg2());
-                    break;
+                    case C_PUSH:
+                    case C_POP:
 
-                case C_CALL:
+                        codeWriter.writePushPop(parser.commandType(),parser.arg1(),parser.arg2());
+                        break;
 
-                    codeWriter.writeCall(parser.arg1(),parser.arg2());
-                    break;
+                    case C_CALL:
 
-                case C_FUNCTION:
+                        codeWriter.writeCall(parser.arg1(),parser.arg2());
+                        break;
 
-                    codeWriter.setCurrentFunction(parser.arg1());
-                    codeWriter.writeFunction(parser.arg1(),parser.arg2());
-                    break;
+                    case C_FUNCTION:
 
-                case C_RETURN:
+                        codeWriter.setCurrentFunction(parser.arg1());
+                        codeWriter.writeFunction(parser.arg1(),parser.arg2());
+                        break;
 
-                    codeWriter.writeReturn();
-                    break;
+                    case C_RETURN:
 
-                case C_LABEL:
+                        codeWriter.writeReturn();
+                        break;
 
-                    codeWriter.writeLabel(parser.arg1());
-                    break;
+                    case C_LABEL:
 
-                case C_GOTO:
+                        codeWriter.writeLabel(parser.arg1());
+                        break;
 
-                    codeWriter.writeGoto(parser.arg1());
-                    break;
+                    case C_GOTO:
 
-                case C_IF:
+                        codeWriter.writeGoto(parser.arg1());
+                        break;
 
-                    codeWriter.writeIf(parser.arg1());
-                    break;
+                    case C_IF:
 
-                default:
+                        codeWriter.writeIf(parser.arg1());
+                        break;
 
-                    break;
+                    default:
+
+                        break;
+
+                }
 
             }
 
-        }
+            parser.close();
 
-        parser.close();
+        }
 
     }
 
     /**
      * Translates and writes the hack code for
-     * a given input directory or vm file
+     * a given input directory or vm file. Called
+     * from main after it creates a file object
+     * for the input.
      *
      * @param input the File object for the input
      *              directory or input vm file
      */
     public void translate(File input) {
 
-        validateExtensions(input);
-        File outputFile = getOutputFile(input);
+        File outputFile = validateInputAndGetOutputFile(input);
 
         CodeWriter codeWriter = new CodeWriter(outputFile);
 
@@ -176,12 +182,6 @@ public class VMTranslator {
 
             // list of files to process
             File[] listOfInputFiles = input.listFiles();
-
-            if(listOfInputFiles==null) {
-                System.out.println("Error: The input directory is empty");
-                System.out.println("Exiting");
-                System.exit(1);
-            }
 
             // translate each file in the directory
             for(File file:listOfInputFiles) {
