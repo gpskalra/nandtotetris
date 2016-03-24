22183bcb1e32a1682ef070a4b6a7f8e3fbf14d0e
gaganpreet1810 <gaganpreet1810@gmail.com>
2016-03-24 15:02:18 +0530

    dont follow complete calling protocol for Sys.init

--- src/com/nandtotetris/vmtranslator/CodeWriter.java
+++ src/com/nandtotetris/vmtranslator/CodeWriter.java
@@ -152,7 +152,21 @@ public class CodeWriter {
         mOutputFile.flush();
 
         // call Sys.init (defined in the file Sys.vm)
-        writeCall("Sys.init",0);
+        // writeCall("Sys.init",0);
+
+        // Since Sys.init never returns, do not need to
+        // do not need to follow the complete protocol for
+        // calling Sys.init
+
+        // LCL=SP
+        mOutputFile.println("@SP");
+        mOutputFile.println("D=M");
+        mOutputFile.println("@LCL");
+        mOutputFile.println("M=D");
+
+        // jump to Sys.init
+        mOutputFile.println("@Sys.init");
+        mOutputFile.println("0;JMP");
 
         // write Sys.init function
         // writeFunction("Sys.init",0);
